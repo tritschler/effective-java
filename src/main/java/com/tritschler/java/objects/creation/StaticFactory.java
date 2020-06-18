364a71c3a2bed302d48881fe43b76e70b4a1a7ff
tritschler <marc_tritschler@yahoo.com>
2020-06-18 15:28:43 +0200

    temp

--- src/main/java/com/tritschler/java/objects/creation/StaticFactory.java
+++ src/main/java/com/tritschler/java/objects/creation/StaticFactory.java
@@ -1,4 +1,31 @@
 package com.tritschler.java.objects.creation;
 
-public class StaticFactory {
+/**
+ * Item 1 : consider using static factory methods
+ * Abstract:
+ *
+ *
+ * Adavntages
+ *  1. have a name compared to
+ *  2.
+ *  3.
+ *  4.
+ *
+ */
+
+public class StaticFactory {}
+
+
+class Car {
+
+
+
+    // from
+    public static Car from(String carType) {
+        return new Car();
+    }
+    // of
+
+    // getInstance
+
 }
