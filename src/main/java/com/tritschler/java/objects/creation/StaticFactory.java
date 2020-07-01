4b99b6c6656bf75a2afc0e433aa696184bb5db9a
tritschler <marc_tritschler@yahoo.com>
2020-07-01 12:59:20 +0200

    StaticFactory class

--- src/main/java/com/tritschler/java/objects/creation/StaticFactory.java
+++ src/main/java/com/tritschler/java/objects/creation/StaticFactory.java
@@ -1,5 +1,9 @@
 package com.tritschler.java.objects.creation;
 
+import java.math.BigInteger;
+
+import static java.math.BigInteger.ONE;
+
 /**
  * Item 1 : consider using static factory methods (instead of constructors)
  * Abstract:
@@ -20,16 +24,21 @@ class Car {
 
     private static Car INSTANCE = new Car();
 
-    // from
+    // common static method names : from, of, valueOf, getInstance
+
+    // from : a type conversion taking a single parameter and returning this type
     public static Car from(String carType) {
 
         return new Car();
     }
 
-    // of
+    // of : an aggregation method
     public static Car of() {
         return new Car();
     }
+
+    // valueOf : a more verbose
+    BigInteger maxInt = BigInteger.valueOf(Integer.MAX_VALUE);
 
     // getInstance
     public static Car getInstance() {
